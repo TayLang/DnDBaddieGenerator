67cbf61704ac3afedf89b0d2bf65ba5563218256
Taylor Lang <lang.taylor.j@gmail.com>
2017-07-24 15:42:43 -0500

    initial commit, schema and readme

---
+++ README.md
@@ -0,0 +1,30 @@
+WELCOME TO THE 5E D&D BADDIE GENERATOR
+
+As a pen and paper RPG enthusiast, I've noticed that many Dungeon Masters like to create their own monsters for player characters to fight. This tool is designed to streamline that process and let you pull up the baddie whenever you want-no more holding on to a binder full of crumpled papers and half-finished drawings! I envision this as being a progression of versions, slowly incorporating more and more logic to automate stuff like relevant stat generation(like version 1 may have you input its initiative modifier, but version 2 may generate that for you based on its parent ability score). 
+
+HOW DO I USE THIS?
+
+Fill out the form as much or as little as you like-only sections marked with a * are required for your input. Some things will have a natural default, like we assume most enemies you'll run across are ambulatory, so you won't need to specify that unless it's a rooted plant monster with no legs, for example. Once you submit the form, that monster gets saved in our database, and you'll be able to pull it back up using the "Find your Monster!" search bar. It's pretty much that simple, for this very early alpha version. Future versions will let you save monsters to your own repository(so you don't have to sift through nine different "Legendary Chrono-Goblin" variants).
+
+WAIT, WHAT MAKES THIS DIFFERENT FROM...
+
+As I was building this, I realized this was basically a character sheet like for any regular PC, but I'm hoping that the way that it's presented will make it more intuitive to think about how you create a bad guy. You could theoretically use this to build any character in 5E, but it's optimized for things that wanna kill you.
+
+FUTURE VERSIONS WILL INCLUDE
+
+I'm hoping that with every like, two or three functions I add, I'll release a mini-update. 
+
+Autogenerating scores that are dependent on other scores(like initiative)
+Statistics of equipment and applying them to battle stats
+Dice roll generator
+Importing images(drawings of your monsters)
+
+I'll do my best to save monsters created across versions while I'm going through alpha and beta versions, but I know that once I add personal monster repositories, that will be a clean slate. 
+
+TECH USED
+
+This is a MERN stack app-MongoDB, Express.js, React.js, and Node.js. 
+
+Thanks to- Hank Holcomb, Meghan B., Adam Ray, Zachary Olson
+
+SPECIAL THANKS TO- Earl Renken II
---
+++ db/schema.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+
+// ----------------------
+// USERS
+// ----------------------
+const usersSchema = new mongoose.Schema({
+  // required for authentication: DO NOT TOUCH Or You May Get Punched
+  email:     { type: String, required: true },
+  password:  { type: String, required: true },
+  // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
+  
+   // example of optional fields
+  name:      { type: String },
+  createdAt: { type: Date, default: Date.now }
+
+})
+
+ const monsterSchema = new mongoose.Schema({
+ 		size: { type: String, enum: ['Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan'], required: true },
+ 		type: { type: String, enum: ['Aberration', 'Beast', 'Celestial', 'Construct', 'Dragon', 'Elemental', 'Fey', 'Fiend', 'Giant', 'Humanoid', 'Monstrosity', 'Ooze', 'Plants', 'Undead'], required: true },
+ 		tag: { type: String },
+ 		alignment: { type: String, enum: ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil', 'Unaligned'], required: true },
+ 		abilityScore: { 
+ 			Strength : { type: Number, min: 1, max: 20, required: true },
+ 			Dexterity : { type: Number, min: 1, max: 20, required: true },
+ 			Constitution : { type: Number, min: 1, max: 20, required: true},
+ 			Intelligence : { type: Number, min: 1, max: 20, required: true},
+ 			Wisdom : { type: Number, min: 1, max: 20, required: true},
+ 			Charisma : { type: Number, min: 1, max: 20, required: true}
+ 		},
+ 		mvmtModes: {
+ 			canWalk : { type: Boolean, default: true},
+ 			canRun : { type: Boolean, default: true},
+ 			canBurrow : { type: Boolean, default: false},
+ 			canSwim : { type: Boolean, default: true},
+ 			canClimb : { type: Boolean, default: true},
+ 			canFly : { type: Boolean, default: false},
+ 		},
+ 		skills: {
+ 			athletics : { type: Number, default: 0},
+ 			acrobatics : { type: Number, default: 0},
+ 			sleightOfHand : { type: Number, default: 0},
+ 			stealth : { type: Number, default: 0},
+ 			arcana : { type: Number, default: 0},
+ 			history : { type: Number, default: 0},
+ 			investigation : { type: Number, default: 0},
+ 			nature : { type: Number, default: 0},
+ 			religion : { type: Number, default: 0},
+ 			animalHandling : { type: Number, default: 0},
+ 			insight : { type: Number, default: 0},
+ 			medicine : { type: Number, default: 0},
+ 			perception : { type: Number, default: 0},
+ 			survival : { type: Number, default: 0},
+ 			deception : { type: Number, default: 0},
+ 			intimidation : { type: Number, default: 0},
+ 			performance : { type: Number, default: 0},
+ 			persuasion : { type: Number, default: 0}
+ 		},
+ 		damType: {
+ 			acid : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			bludgeoning : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			cold : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			fire : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			force : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			lightning : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			necrotic : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			piercing : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			poison : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			psychic : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			radiant : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			slashing : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 			thunder : { type: String, enum: ['isVul', 'isRes', 'isImm', 'none'], default: 'none'
+ 			},
+ 		},
+ 		senses : {
+ 			hasBlindsight: {type: Boolean},
+ 			hasDarkvision: {type: Boolean},
+ 			hasTremorsense: {type: Boolean},
+ 			hasTruesight: {type: Boolean}
+ 		},
+ 		languages: {
+ 			canSpeak: {
+ 				type: String, enum: ['abyssal', 'aquan', 'auran', 'celestial', 'common', 'deepSpeech', 'draconic', 'druidic', 'dwarvish', 'elvish', 'giant', 'gnomish', 'goblin', 'gnoll', 'halfling', 'ignan', 'infernal', 'orc', 'primordial', 'sylvan', 'terran', 'undercommon', 'none']
+ 			},
+ 			canComprehend: {
+ 				type: String, enum: ['abyssal', 'aquan', 'auran', 'celestial', 'common', 'deepSpeech', 'draconic', 'druidic', 'dwarvish', 'elvish', 'giant', 'gnomish', 'goblin', 'gnoll', 'halfling', 'ignan', 'infernal', 'orc', 'primordial', 'sylvan', 'terran', 'undercommon', 'none']
+ 			},
+ 			hasTelepathy: {type: Boolean, default: false}
+ 		},
+ 		environment: {type: String},
+ 		challengeRating: {type: Number, required: true},
+ 		reactions: {type: String},
+ 		attacks: {type: Array},
+ 		specialAbilities: {type: String},
+ 		equipment: {type: Array},
+
+
+ })
+
+module.exports = {
+  User: mongoose.model('User', usersSchema)
+}
